87e9f98eaa09e2a57e6c5e08b61aeffbe21f0a7b
Laksito Pamilih <laksito@protonmail.com>
2023-04-29 22:58:06 +0700

    add head title; add padding to main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import { supabase } from './../lib/supabaseClient'
 import { useEffect } from 'react'
@@ -37,57 +38,61 @@ export default function Home({ groups }: Props) {
   }, [groups])
 
   return (
-    <main
-
-    >
-      <div>
-        {
-          groups.map((group) => (
-            <div key={group.id}>
-              <h3 className="mb-2">{group.title}</h3>
-              <ul className="mb-5">
-                {
-                  group.albums.map((album) => (
-                    <li
-                      key={group.id + album.id}
-                      className="pl-6"
-                    >
-                      <span className={album.is_highlighted ? 'font-bold' : ''}>
-                        <span>
-                          {album.title}
-                        </span>
-                        <span> - </span>
-                        <span>
-                          {album.artists.map(artist => artist.name).join(', ') || 'n/a'}
+    <>
+      <Head>
+        <title>Nusantaracore</title>
+      </Head>
+      <main
+        className="p-4"
+      >
+        <div>
+          {
+            groups.map((group) => (
+              <div key={group.id}>
+                <h3 className="mb-2">{group.title}</h3>
+                <ul className="mb-5">
+                  {
+                    group.albums.map((album) => (
+                      <li
+                        key={group.id + album.id}
+                      >
+                        <span className={album.is_highlighted ? 'font-bold' : ''}>
+                          <span>
+                            {album.title}
+                          </span>
+                          <span> - </span>
+                          <span>
+                            {album.artists.map(artist => artist.name).join(', ') || 'n/a'}
+                          </span>
+                          {
+                            album.year && <span> ({ album.year })</span>
+                          }
                         </span>
                         {
-                          album.year && <span> ({ album.year })</span>
+                          album.youtube && <a href={album.youtube} target="__blank" className=" text-blue-800">&nbsp;<span className="underline">youtube</span></a>
                         }
-                      </span>
-                      {
-                        album.youtube && <a href={album.youtube} className=" text-blue-800">&nbsp;<span className="underline">youtube</span></a>
-                      }
-                      {
-                        album.spotify && <a href={album.spotify} className=" text-blue-800">&nbsp;<span className="underline">spotify</span></a>
-                      }
-                      {/* {
-                        album.apple && <a href={album.apple} className=" text-blue-800">&nbsp;<span className="underline">apple</span></a>
-                      } */}
-                    </li>
-                  ))
-                }
-              </ul>
-            </div>
-          )) 
-        }
-      </div>
+                        {
+                          album.spotify && <a href={album.spotify} target="__blank" className=" text-blue-800">&nbsp;<span className="underline">spotify</span></a>
+                        }
+                        {/* {
+                          album.apple && <a href={album.apple} className=" text-blue-800">&nbsp;<span className="underline">apple</span></a>
+                        } */}
+                      </li>
+                    ))
+                  }
+                </ul>
+              </div>
+            )) 
+          }
+        </div>
 
-      <div className="m-6 px-4 py-3 border">
-        twitter: @fthrrhmn31
-        <br/>
-        spotify: Aditya Fathurrahman
-      </div>
-    </main>
+        <div className="px-4 py-3 border">
+          twitter: @fthrrhmn31
+          <br/>
+          spotify: Aditya Fathurrahman
+        </div>
+      </main>
+    </>
   )
 }
 
