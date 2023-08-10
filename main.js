48fd0ffaf822e34622b41507d168c8b299f1484d
Laksito Pamilih <laksito@protonmail.com>
2023-08-10 14:35:00 +0700

    copy public files

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import 'dotenv/config'
+import copyPublicFiles from './lib/copyPublicFiles.js'
 import getData from './lib/getData.js'
 import generateMd from './lib/generateMd.js'
 import generateHtml from './lib/generateHtml.js'
 
+await copyPublicFiles()
 const data = await getData()
 const mdPath = await generateMd(data)
 generateHtml(mdPath)
