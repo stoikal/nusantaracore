2161e1a1346c57ef15eefd2d31aa0322beb98e16
Laksito Pamilih <laksito@protonmail.com>
2023-08-10 14:33:18 +0700

    add config file

---
+++ config.js
@@ -0,0 +1,8 @@
+export default {
+  SUPABASE_URL: process.env.SUPABASE_URL,
+  SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+  DOCUMENT_TITLE: 'Nusantaracore',
+  MD_OUPUT_PATH: 'README.md',
+  BUILD_DIR: 'build',
+  PUBLIC_DIR: 'public'
+}
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import getData from './lib/getData.js'
 import generateMd from './lib/generateMd.js'
 import generateHtml from './lib/generateHtml.js'
